e057a13137a03bddaa1e3883caa400d523347c69
Tarskan <55380090+Tarskan@users.noreply.github.com>
2022-10-03 16:42:22 +0200

    push 4 3102022

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -4,16 +4,29 @@ using System;
 using System.Diagnostics;
 using Leonardo;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");  
 
-IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddEnvironmentVariables()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
-    .Build();    
+IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+    .AddEnvironmentVariables().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true).Build();
+
+var applicationSection = configuration.GetSection("Application");
+var applicationConfig = applicationSection.Get<ApplicationConfig>();
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{ 
+    builder.AddFilter("Microsoft", LogLevel.Warning).AddFilter("System", LogLevel.Warning).AddFilter("Demo", LogLevel.Debug).AddConsole();
+});
 
 var applicationName = configuration.GetValue<string>("Application:Name");    
-var applicationMessage = configuration.GetValue<string>("Application:Message");   
+var applicationMessage = configuration.GetValue<string>("Application:Message");
+
+var logger = loggerFactory.CreateLogger("Demo.Program");
+
+logger.LogInformation($"Application Name : {applicationConfig.Name}");
+logger.LogInformation($"Application Message : {applicationConfig.Message}");
 
 Console.WriteLine($"Application Name : {applicationName}");    
 Console.WriteLine($"Application Message : {applicationMessage}");
@@ -32,5 +45,3 @@ foreach (var listOfResult in listOfResults)
 stopwatch.Stop();
 
 Console.WriteLine("time elapsed in seconds : " + stopwatch.Elapsed.Seconds);
-
-
