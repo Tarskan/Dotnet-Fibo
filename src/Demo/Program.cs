1a42cce0229e5d69a03af578d4a1a3660cfe64ab
Tarskan <55380090+Tarskan@users.noreply.github.com>
2022-10-03 15:36:53 +0200

    push 2 3102022

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -3,6 +3,20 @@
 using System;
 using System.Diagnostics;
 using Leonardo;
+using Microsoft.Extensions.Configuration;
+
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");  
+
+IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddEnvironmentVariables()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+    .Build();    
+
+var applicationName = configuration.GetValue<string>("Application:Name");    
+var applicationMessage = configuration.GetValue<string>("Application:Message");   
+
+Console.WriteLine($"Application Name : {applicationName}");    
+Console.WriteLine($"Application Message : {applicationMessage}");
 
 var stopwatch = new Stopwatch();
 
